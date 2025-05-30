1fdc62cf643df2d508c5a2faac8aac5845633870
huangxu <xu.huang@my.jcu.edu.au>
2025-05-30 18:01:58 +0800

    Add two functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,3 +1,12 @@
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
+
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return celsius * 9 / 5 + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5 /9
+
