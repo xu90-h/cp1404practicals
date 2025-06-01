b136b0adcc0e1baba570d1426c7906245f3a8322
huangxu <xu.huang@my.jcu.edu.au>
2025-06-01 17:10:01 +0800

    Move the main function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,9 @@
 MIN_LENGTH = 8
 
+def main():
+    password = get_password(MIN_LENGTH)
+    print(print_stars(password))
+
 def get_password(MIN_LENGTH):
     """Set the valid password length to be at least 'min_length' characters"""
     password = input("Enter password: ")
@@ -9,11 +13,6 @@ def get_password(MIN_LENGTH):
 
 def print_stars(password):
     print("*" * len(password))
-
-def main():
-    password = get_password(MIN_LENGTH)
-    print(print_stars(password))
-
 
 if __name__ == '__main__':
     main()
