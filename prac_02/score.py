a81cff24e26bbad1476dfe9d23fdfa3eb3f22c97
huangxu <xu.huang@my.jcu.edu.au>
2025-06-01 15:28:34 +0800

    Add two function

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,4 +3,22 @@ import random
 MIN_SCORE = 0
 MAX_SCORE = 100
 
+def user_score(score):
+    """Return the result according to score"""
+    if score < MIN_SCORE or score > MAX_SCORE:
+        return "Invalid score."
+    elif score >= 90:
+        return "Excellent."
+    elif score >= 50:
+        return "Pass."
+    else:
+        return "Bad."
+
+def main():
+    """Get the user's score, display the result and display the random score"""
+    score = float(input("Enter score: "))
+    print(score)
+    random_score = random.randint(MIN_SCORE, MAX_SCORE)
+    print("Random score is: ", random_score)
+    print(random_score)
 
