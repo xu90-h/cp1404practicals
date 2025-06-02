1c5b79084f4bc6baa8d6b591c23ed305a9170ee2
huangxu <xu.huang@my.jcu.edu.au>
2025-06-02 14:48:10 +0800

    Explain the problem as required

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,5 +1,13 @@
 import random
 
 print(random.randint(5, 20))  # line 1
+# Return an integer including both ends.
+# The minimum value is 5 and the maximum value is 20
+
 print(random.randrange(3, 10, 2))  # line 2
+# Return a value
+# The minimum value is 3 and the maximum value is 9.
+
 print(random.uniform(2.5, 5.5))  # line 3
+# Return a random number of type float.
+# The minimum value is 2.5 and the maximum value is 5.5.
