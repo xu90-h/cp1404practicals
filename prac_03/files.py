52c8bfacaed37d708b3f7fc51f49072be9fb4f47
huangxu <xu.huang@my.jcu.edu.au>
2025-06-08 13:51:30 +0800

    Add greeting

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,10 @@ name = input("Enter your name: ")
 file = open('name.txt', 'w')
 file.write(name)
 file.close()
+
+# Question 2: Read name from name.txt and greet
+file = open('name.txt', 'r')
+name = file.read()
+print(f"Hi! {name}")
+file.close()
+
