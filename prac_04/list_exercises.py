a2a91f875027494fb23ea3a01b03f974ee084315
huangxu <xu.huang@my.jcu.edu.au>
2025-06-14 14:09:40 +0800

    Add username access check

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -14,3 +14,8 @@ print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 # Woefully inadequate security checker
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
