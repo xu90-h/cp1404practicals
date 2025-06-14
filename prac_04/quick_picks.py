38532cbd28b715cce1c671d65c28ae949329fd0a
huangxu <xu.huang@my.jcu.edu.au>
2025-06-14 17:43:53 +0800

    main function as first function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,15 +4,6 @@ MIN_NUMBER = 1
 MAX_NUMBER = 45
 NUMBERS_PER_PICK = 6
 
-def quick_pick():
-    numbers = []
-    while len(numbers) < NUMBERS_PER_PICK:
-        number = random.randint(MIN_NUMBER, MAX_NUMBER)
-        if number not in numbers:
-            numbers.append(number)
-    numbers.sort()
-    return numbers
-
 def main():
     try:
         quick_picks = int(input("How many quick picks?"))
@@ -25,5 +16,14 @@ def main():
     for _ in range(quick_picks):
         pick = quick_pick()
         print(" ".join(f"{num:2}" for num in pick))
+
+def quick_pick():
+    numbers = []
+    while len(numbers) < NUMBERS_PER_PICK:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in numbers:
+            numbers.append(number)
+    numbers.sort()
+    return numbers
 
 main()
