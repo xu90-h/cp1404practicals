1faed7a1e33468d40775f5bb0ab51de4c83275ad
huangxu <xu.huang@my.jcu.edu.au>
2025-06-14 14:22:25 +0800

    Convert string numbers to integers using list comprehension

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -32,8 +32,8 @@ print(" ".join(sorted(names)))
 lowercase_full_names = [name.lower() for name in full_names]
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
-# TODO: list comprehension to create a list of integers from the above list of strings
-# numbers =
+numbers = [int(num) for num in almost_numbers]
+print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
