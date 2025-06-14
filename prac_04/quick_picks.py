0f50084288a97f936c533b4edaaaf5977b56b7ce
huangxu <xu.huang@my.jcu.edu.au>
2025-06-14 16:32:05 +0800

    Covert quick pick number generation

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,3 +4,11 @@ MIN_NUMBER = 1
 MAX_NUMBER = 45
 NUMBERS_PER_PICK = 6
 
+def quick_pick():
+    numbers = []
+    while len(numbers) < NUMBERS_PER_PICK:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in numbers:
+            numbers.append(number)
+    numbers.sort()
+    return numbers
