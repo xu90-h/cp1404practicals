054612ac54676e535e12908e33e52a4eb529923d
huangxu <xu.huang@my.jcu.edu.au>
2025-06-15 13:33:46 +0800

    Change the program so lowercase inputs also work to show the state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -17,7 +17,7 @@ CODE_TO_NAME = {
 
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
