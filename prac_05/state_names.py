192647c6f744614f843be8e1f849581225c95475
huangxu <xu.huang@my.jcu.edu.au>
2025-06-15 13:38:09 +0800

    EAFP

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -19,8 +19,8 @@ print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
