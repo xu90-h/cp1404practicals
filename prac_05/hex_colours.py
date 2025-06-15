ec21c58017bf8973ce09597a6fc3755d9e2a7c60
huangxu <xu.huang@my.jcu.edu.au>
2025-06-15 13:48:51 +0800

    Add color code for colors.

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+CODE_TO_NAME = {
+    "AliceBlue": "#f0f8ff",
+    "Coral": "#ff7f50",
+    "Crimson": "#dc143c",
+    "DarkOrange": "#ff8c00",
+    "Gold": "#ffd700",
+    "HotPink": "#ff69b4",
+    "Indigo": "#4b0082",
+    "LimeGreen": "#32cd32",
+    "MediumPurple": "#9370db",
+    "Navy": "#000080"
+}
