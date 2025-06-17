6949661869b616c5132edfbf16c578788b9f39db
huangxu <xu.huang@my.jcu.edu.au>
2025-06-17 15:44:13 +0800

    Align output

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -16,3 +16,8 @@ def main():
             word_to_count[word] = 1
 
     sorted_words = sorted(word_to_count.keys())
+    max_length = max(len(word) for word in sorted_words)
+    for word in sorted_words:
+        print(f"{word:{max_length}} : {word_to_count[word]}")
+
+main()
