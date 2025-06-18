a17700b542fbdc097e8f31a7b2344799973b2528
huangxu <xu.huang@my.jcu.edu.au>
2025-06-18 16:24:01 +0800

    Add basic email input loop and dictionary storage

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,11 @@ Program to store users' emails and names in a dictionary.
 Estimate: 20 minutes
 Actual:
 """
+
+def main():
+    """Get emails and extract names into a dictionary."""
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        email_to_name[email] = ""
+        email = input("Email: ")
