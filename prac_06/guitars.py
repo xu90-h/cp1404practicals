8683cb84cbc93fc1829eed39e1982b6b27c60c66
huangxu <xu.huang@my.jcu.edu.au>
2025-07-08 13:12:13 +0800

    Display guitars with formatted output and vintage label

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -17,6 +17,17 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+    if guitars:
+        guitars.sort()
+        print("These are my guitars: ")
 
+        for i, guitar in enumerate(guitars, 1):
+            # enumerate object
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("No guitars :(Quick, go and buy one!)")
 
 main()
