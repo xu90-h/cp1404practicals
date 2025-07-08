bcc771d919056123d05d01a96735523b9d7df79c
huangxu <xu.huang@my.jcu.edu.au>
2025-07-08 13:11:53 +0800

    Add predefined test guitars for display

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -13,6 +13,10 @@ def main():
         print(f"{guitar_add} added.")
         name = input("Name: ")
 
+    guitars.append(Guitar("Fender Stratocruisers", 2014, 765.40 ))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
 
 
 main()
