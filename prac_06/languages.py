d6a99128b71b10a83465e9f4ebeb1adc286614ad
huangxu <xu.huang@my.jcu.edu.au>
2025-07-08 11:18:11 +0800

    Improve

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,13 +1,16 @@
 from programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
+def main():
 
-languages = [python, ruby, visual_basic]
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-print("The dynamically typed languages are:")
-for language in languages:
-    if language.is_dynamic():
-        print(language.name)
+    languages = [python, ruby, visual_basic]
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+main()
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,12 +1,12 @@
 class ProgrammingLanguage:
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name=" ", typing=" ", is_reflection=False, year= 0):
         self.name = name
         self.typing = typing
-        self.reflection = reflection
+        self.is_reflection = is_reflection
         self.year = year
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
 
     def __str__(self):
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.is_reflection}, First appeared in {self.year}"
