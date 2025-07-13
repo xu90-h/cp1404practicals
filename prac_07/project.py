6fd6ac2e5d0a497a66dba48b38d8ff80270eccf1
huangxu <xu.huang@my.jcu.edu.au>
2025-07-13 16:09:15 +0800

    Add Project class foundation

---
+++ prac_07/project.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+class Project:
+    """Display project information."""
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
