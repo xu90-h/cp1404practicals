141f5d9836ccc1b70f8479434669723ddbe61b05
huangxu <xu.huang@my.jcu.edu.au>
2025-07-13 15:48:46 +0800

    Complete main program to load and display guitars

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,5 +1,11 @@
 from guitar import Guitar
 
+def main():
+    filename = "guitars.csv"
+    guitars = load_guitars(filename)
+    print("These are my guitars:")
+    display_guitars(guitars)
+
 def load_guitars(filename):
     """Read guitars from a CSV file and return a list of Guitar objects."""
     guitars = []
@@ -17,3 +23,5 @@ def display_guitars(guitars):
     """Display a list of Guitar objects with numbering."""
     for i, guitar in enumerate(guitars, 1):
         print(f"Guitar {i}: {guitar}")
+
+main()
