1edb07d7a72f0ee2c42ed2c692992488a9aa4314
huangxu <xu.huang@my.jcu.edu.au>
2025-07-13 16:11:43 +0800

    Add string representation and comparison to Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,3 +8,15 @@ class Project:
         self.priority = int(priority)
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = int(completion_percentage)
+
+    def __str__(self):
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
+
+    def is_complete(self):
+        """Check if the project has been completed."""
+        return self.completion_percentage >= 100
+
+    def __lt__(self, other):
+        """Define priority comparison."""
+        return self.priority < other.priority
