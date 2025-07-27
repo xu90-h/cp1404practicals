93f2269412a4a613deed161561b1f90dab1c4507
huangxu <xu.huang@my.jcu.edu.au>
2025-07-27 14:16:25 +0800

    Test silver_service_taxi_test.py

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,8 @@
+from silver_service_taxi import SilverServiceTaxi
+
+def main():
+    """Test the class SilverServiceTaxi's method"""
+
+    #define a taxi with fanciness
+    fancy_taxi = SilverServiceTaxi("Test taxi", 100, 2)
+    fancy_taxi.drive(18)
