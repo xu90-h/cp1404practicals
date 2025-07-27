ec12b6bf24722f1011936361dc9c21c85c62af5e
huangxu <xu.huang@my.jcu.edu.au>
2025-07-27 13:32:49 +0800

    Add the reliability parameter in init

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,3 +1,16 @@
 import random
 from random import randint
 from car import Car
+
+class UnreliableCar(Car):
+    """A special car that does not drive reliably"""
+
+    def __init__(self,name, fuel, reliability):
+        """Initialize an UnreliableCar with a random reliability percentage
+        Args:
+            name(str): name of car
+            fuel(int): fuel of car
+            reliability(int) : 0-100 percentage
+        """
+        super().__init__(name, fuel)
+        self.reliability = reliability
