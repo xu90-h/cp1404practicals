905d49bbf350c7302e7be988a0ef9b26043573d2
huangxu <xu.huang@my.jcu.edu.au>
2025-07-27 14:36:16 +0800

    Complete homework

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -15,5 +15,5 @@ def main():
     # use assert to check if correct
     assert round(fancy_taxi.get_fare(), 1) == 48.80, "Calculation is not correct"
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
