5def58f903dbf431033e22cd09aef2c948f43ff1
huangxu <xu.huang@my.jcu.edu.au>
2025-07-27 13:06:18 +0800

    Complete exercise

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -15,3 +15,12 @@ my_taxi.drive(40)
 #print current fare
 print(my_taxi)
 print(f"The current fare is {my_taxi.get_fare():.2f}")
+
+#restart the meter
+my_taxi.start_fare()
+#drive 100 kms
+my_taxi.drive(100)
+
+#print details and current fare
+print(my_taxi)
+print(f"The current fare is {my_taxi.get_fare():.2f}")
