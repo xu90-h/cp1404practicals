158bf4d6338e76bfdcd4fbdc777c45533ad3c805
huangxu <xu.huang@my.jcu.edu.au>
2025-07-27 13:05:35 +0800

    Drive 40 kms

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,3 +2,16 @@
 EST:25 mins
 ACT:
 """
+
+#import class Taxi
+from taxi import Taxi
+
+#create new object
+my_taxi = Taxi("Prius 1",100)
+
+#drive taxi 40 kms
+my_taxi.drive(40)
+
+#print current fare
+print(my_taxi)
+print(f"The current fare is {my_taxi.get_fare():.2f}")
