2b7b5a0f77bcb3cc31a934bb747959e8a076ef54
huangxu <xu.huang@my.jcu.edu.au>
2025-07-27 16:19:14 +0800

    add __init__

---
+++ prac_09/band.py
@@ -0,0 +1,7 @@
+class Band:
+    """Class Band, a band with multiple musicians"""
+
+    def __init__(self,name):
+        """Initialize a Band class"""
+        self.name = name
+        self.musicians = []
