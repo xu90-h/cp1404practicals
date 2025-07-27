6bfe11931aa6ab62cdd17a474141a2d1046c4bbb
huangxu <xu.huang@my.jcu.edu.au>
2025-07-27 15:07:53 +0800

    Add part of main()

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,3 +8,12 @@ from car import Car
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+def main():
+    """A taxi simulator program matches readme.md's requirements"""
+    total_bill = 0
+    current_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo",100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    print(MENU)
