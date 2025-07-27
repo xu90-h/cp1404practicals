24f431159caf4c29c0afaa64cd1e033db9ad9066
huangxu <xu.huang@my.jcu.edu.au>
2025-07-27 14:11:43 +0800

    Add get_fare

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -13,3 +13,7 @@ class SilverServiceTaxi(Taxi):
     def __str__(self):
         """Return a string representation of a SilverServiceTaxi"""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """get current fare"""
+        return self.flagfall + super().get_fare()
