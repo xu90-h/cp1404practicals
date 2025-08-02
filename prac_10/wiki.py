29699267110e6c767454c2085640f8e2977209cd
huangxu <xu.huang@my.jcu.edu.au>
2025-08-02 14:07:45 +0800

    Implement for wiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,3 +8,16 @@ def main():
         if not title:
             print("Thank you.")
             break
+
+    try:
+        page = wikipedia.page(title, auto_suggest=False)
+        print(page.title)
+        print(page.summary)
+        print(page.url)
+    except DisambiguationError:
+        print("We need a more specific title. Try one of the following, or a new search:")
+        print(DisambiguationError.options[:5], "...")
+    except PageError:
+        print(f"Page id {title} does not match any pages. Try another id!")
+
+    main()
