36814da2ff630a09572d5dbe33d9f8c3e78b98f4
huangxu <xu.huang@my.jcu.edu.au>
2025-08-02 14:07:16 +0800

    implement a loop that continues doing until the user enters blank input.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,3 +1,10 @@
 import wikipedia
 from wikipedia.exceptions import DisambiguationError, PageError
 
+def main():
+    """A function to try wikipedia"""
+    while True:
+        title = input("Enter title page:").strip()
+        if not title:
+            print("Thank you.")
+            break
